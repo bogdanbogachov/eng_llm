f3ca9bb571a18ccf48502ed4264963e94bb7dbae
Bod <bogdan.bogachov@protonmail.com>
2025-01-28 17:42:33 +0100

    Edited slg.py

--- inference/slg.py
+++ inference/slg.py
@@ -29,12 +29,31 @@ class SmallLanguageGraph:
         total = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
         llm_response = client.text_generation(
             total,
-            model=CONFIG['model_id'],
+            model=CONFIG['3_1_8b'],
             max_new_tokens=CONFIG['max_new_tokens'],
             seed=CONFIG['seed'],
             temperature=CONFIG['temperature']
         )
+
         return llm_response
+
+        # model_id = 'downloaded_3_1_8b'
+        # tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+        #
+        # pipe = pipeline(
+        #     "text-generation",
+        #     model=model_id,
+        #     device_map="auto",
+        # )
+        #
+        # outputs = pipe(
+        #     messages,
+        #     max_new_tokens=CONFIG['max_new_tokens'],
+        #     temperature=CONFIG['temperature'],
+        #     tokenizer=tokenizer
+        # )
+        #
+        # return outputs[0]["generated_text"][-1]['content']
 
     @staticmethod
     def _tuned_generate(prompt, model):
@@ -69,7 +88,7 @@ class SmallLanguageGraph:
         """Analyze the task and route to appropriate expert."""
         question = state["question"]
         experts_list = "\n".join(
-            f"- {expert}" for expert in os.listdir(f'experiments/{self.experts_location}'))
+            f"- {expert}" for expert in os.listdir(f'experiments/{self.experts_location}/slg'))
         prompt = (
             f"Analyze the question below and find an expert from the following list which can answer the question:\n"
             f"{experts_list}\n"
@@ -87,13 +106,13 @@ class SmallLanguageGraph:
         Provide a detailed answer to the following question: {question}"""
         state["answer"] = self._tuned_generate(
             prompt,
-            f"experiments/{self.experts_location}/{model}"
+            f"experiments/{self.experts_location}/slg/{model}"
         )
         return state
 
     def _routing_function(self, state):
         """Route based on the category identified in the task analysis."""
-        models = {folder_name: folder_name for folder_name in os.listdir(f'experiments/{self.experts_location}')}
+        models = {folder_name: folder_name for folder_name in os.listdir(f'experiments/{self.experts_location}/slg')}
         category = state.get("category")
         return models.get(category, END)
 
@@ -103,7 +122,7 @@ class SmallLanguageGraph:
 
         logger.info("Adding nodes to the graph.")
         graph_builder.add_node("task_analysis", self._task_analysis_node)
-        for node in os.listdir(f'experiments/{self.experts_location}'):
+        for node in os.listdir(f'experiments/{self.experts_location}/slg'):
             if not node.endswith(".json"):
                 logger.info(f"Adding node {node}.")
                 graph_builder.add_node(
@@ -116,7 +135,7 @@ class SmallLanguageGraph:
         logger.info("Adding edges to the graph.")
         graph_builder.add_edge(START, "task_analysis")
         graph_builder.add_conditional_edges("task_analysis", self._routing_function)
-        for edge in os.listdir(f'experiments/{self.experts_location}'):
+        for edge in os.listdir(f'experiments/{self.experts_location}/slg'):
             if not edge.endswith(".json"):
                 logger.info(f"Adding edge {edge}.")
                 graph_builder.add_edge(edge, END)
