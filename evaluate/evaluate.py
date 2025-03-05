54180bc43c8d210b3461da6b63a9491f810a5dcf
Bod <bogdan.bogachov@protonmail.com>
2025-03-04 19:16:53 -0500

    Change AI expert to meteor

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -6,9 +6,15 @@ from config import CONFIG
 from huggingface_hub import InferenceClient
 from transformers import AutoTokenizer
 from huggingface_hub import login
-import time
 import os
 import re
+import nltk
+from nltk.translate.meteor_score import meteor_score
+from nltk.tokenize import word_tokenize
+
+
+# Download WordNet for synonym matching
+nltk.download('wordnet')
 
 logger.propagate = False
 
@@ -32,7 +38,7 @@ def calculate_bleu(reference, candidate):
     reference_tokens = reference.split()  # BLEU expects a list of references
     candidate_tokens = candidate.split()
     smoothing = SmoothingFunction().method4  # For smoothing BLEU scores
-    return sentence_bleu(reference_tokens, candidate_tokens, smoothing_function=smoothing)
+    return sentence_bleu(references=reference_tokens, hypothesis=candidate_tokens, smoothing_function=smoothing)
 
 
 def calculate_rouge(reference, candidate):
@@ -40,7 +46,7 @@ def calculate_rouge(reference, candidate):
     Calculate ROUGE scores between a reference and a candidate answer.
     """
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
-    scores = scorer.score(reference, candidate)
+    scores = scorer.score(target=reference, prediction=candidate)
     return scores
 
 
@@ -87,6 +93,15 @@ def calculate_ai_expert(reference, candidate):
         return 0
 
 
+# Function to calculate METEOR scores
+def calculate_meteor_score(reference, candidate):
+    """
+    Calculate meteor score between a reference and a candidate answer.
+    """
+    score = meteor_score(references=[word_tokenize(reference)], hypothesis=word_tokenize(candidate))
+    return score
+
+
 def evaluate(predictions, ground_truth):
     """
     Evaluate predictions against ground truth using BLEU, ROUGE, and Exact Match.
@@ -94,13 +109,14 @@ def evaluate(predictions, ground_truth):
     bleu_scores = []
     rouge_scores = {'rouge1': [], 'rouge2': [], 'rougeL': []}
     exact_matches = []
-    ai_experts = []
+    # ai_experts = []
+    meteor_scores = []
     logger.info(f"Evaluation has started.")
     counter = 0
     threshold = 10
     for pred, truth in zip(predictions, ground_truth):
-        if counter in [i for i in range(0, len(ground_truth), 1)]:
-            time.sleep(1)
+        # if counter in [i for i in range(0, len(ground_truth), 30)]:
+        #     time.sleep(45)
         counter += 1
         percent_checked = (counter/len(predictions))*100
         if threshold < percent_checked:
@@ -142,25 +158,32 @@ def evaluate(predictions, ground_truth):
         logger.debug("Calculating Exact Match (EM) score.")
         exact_matches.append(calculate_exact_match(gt_answer, pred_answer))
 
-        # AI expert
-        logger.debug("Calculating AI expert score.")
-        ai_expert = calculate_ai_expert(gt_answer, pred_answer)
-        ai_experts.append(ai_expert)
+        # # AI expert
+        # logger.debug("Calculating AI expert score.")
+        # ai_expert = calculate_ai_expert(gt_answer, pred_answer)
+        # ai_experts.append(ai_expert)
+
+        # Meteor
+        logger.debug("Calculating METEOR score.")
+        meteor = calculate_meteor_score(gt_answer, pred_answer)
+        meteor_scores.append(meteor)
 
     # Aggregate scores
     avg_bleu = sum(bleu_scores) / len(bleu_scores) if bleu_scores else 0
     avg_rouge = {key: sum(rouge_scores[key]) / len(rouge_scores[key]) for key in rouge_scores.keys()}
     exact_match_score = sum(exact_matches) / len(exact_matches) if exact_matches else 0
-    ai_expert_score = sum(ai_experts) / len(ai_experts) if ai_experts else 0
+    # ai_expert_score = sum(ai_experts) / len(ai_experts) if ai_experts else 0
+    avg_meteor = sum(meteor_scores) / len(meteor_scores) if meteor_scores else 0
     logger.info(f"Evaluation has been completed.")
     logger.info(40*'-')
-    time.sleep(30)
+    # time.sleep(30)
 
     return {
         "BLEU": avg_bleu,
         "ROUGE": avg_rouge,
         "Exact Match": exact_match_score,
-        "AI Expert": ai_expert_score
+        # "AI Expert": ai_expert_score
+        "METEOR": avg_meteor
     }
 
 
@@ -177,7 +200,8 @@ def extract_log_values(log_file):
         return {
             'train_loss': second_last_log['train_loss'],
             'train_runtime': second_last_log['train_runtime'],
-            'eval_loss': last_log['eval_loss']
+            'eval_loss': last_log['eval_loss'],
+            'epochs': last_log['epoch']
         }
 
 
@@ -189,6 +213,7 @@ def pull_training_metrics(base_folder):
             all_train_loss = 0
             all_eval_loss = 0
             total_train_runtime = 0
+            total_epochs = 0
             count = 0
 
             for subfolder in os.listdir(folder_path):
@@ -201,14 +226,16 @@ def pull_training_metrics(base_folder):
                         all_train_loss += log_values['train_loss']
                         all_eval_loss += log_values['eval_loss']
                         total_train_runtime += log_values['train_runtime']
+                        total_epochs += log_values['epochs']
                         count += 1
 
 
             metrics.append(
                 {
-                'train_loss_slg': all_train_loss / count,
-                'eval_loss_slg': all_eval_loss / count,
-                'train_runtime_slg': total_train_runtime
+                    'avg_train_loss_slg': all_train_loss / count,
+                    'avg_eval_loss_slg': all_eval_loss / count,
+                    'total_train_runtime_slg': total_train_runtime,
+                    'avg_epochs_slg': total_epochs / count
                 }
             )
         elif folder != 'slg' and folder != 'logs':
@@ -220,7 +247,8 @@ def pull_training_metrics(base_folder):
                     {
                         f'train_loss_{folder}': log_values['train_loss'],
                         f'all_eval_loss_{folder}': log_values['eval_loss'],
-                        f'train_runtime_{folder}': log_values['train_runtime']
+                        f'train_runtime_{folder}': log_values['train_runtime'],
+                        f'epochs_{folder}': log_values['epochs']
                     }
                 )
 
